93f6059f671442ddd9ae12d270e43b5ed86639b0
Lukasz Poznanski <l.poznanski3@gmail.com>
2023-01-13 10:44:43 +0100

    Add Room model to booking_app.models

--- reservation/reservation/settings.py
+++ reservation/reservation/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'xxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,8 +79,8 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'NAME': 'booking_db',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
     }
 }
 
